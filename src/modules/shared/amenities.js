5c415d30db6e4f58954b9cbd06d41be88f49bff4
bibekshah220 <bibekshah425@gmail.com>
2026-01-30 19:45:18 +0545

    add common amenities for room rentals

---
+++ src/modules/shared/amenities.js
@@ -0,0 +1,15 @@
+// Common amenities for Room Rentals
+const AMENITIES = [
+  "Wifi",
+  "Parking",
+  "Kitchen",
+  "Water Supply",
+  "Hot Water",
+  "Balcony",
+  "Terrace",
+  "Furnished",
+  "Security",
+  "Garden",
+];
+
+module.exports = { AMENITIES };
